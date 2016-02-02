7680d4cdc2b45b1dc9a54c725fdeaa4917f21312
kantharuban2006 <kanth.ruban@gmail.com>
2016-02-02 14:34:20 +0530

    Update mailParser.js
    Indentation changes.

--- mailParser.js
+++ mailParser.js
@@ -37,47 +37,43 @@ openInbox(function(err, box) {
     if (err) throw err;
     var f = imap.fetch(results, { bodies: ['HEADER.FIELDS (FROM TO SUBJECT DATE)','TEXT'] });
     f.on('message', function(msg, seqno) {
-      //console.log('Message #%d', seqno);
       var prefix = '(#' + seqno + ') ';
 	  
       msg.on('body', function(stream, info) {
         stream.pipe(fs.createWriteStream('msg-' + seqno + '-body.txt'));
 		
-		if (info.which === 'TEXT')
-        console.log(prefix + 'Body [%s] found, %d total bytes', inspect(info.which), info.size);
+	if (info.which === 'TEXT')
+        	console.log(prefix + 'Body [%s] found, %d total bytes', inspect(info.which), info.size);
 		
-		var buffer = '', count = 0;
-		stream.on('data', function(chunk) {
-        count += chunk.length;
-        buffer += chunk.toString('utf8');
-        /*if (info.which === 'TEXT')
-          console.log(prefix + 'Body [%s] (%d/%d)', inspect(info.which), count, info.size);*/
-		});
+	var buffer = '', count = 0;
+	stream.on('data', function(chunk) {
+        	count += chunk.length;
+        	buffer += chunk.toString('utf8');
+       		});
 	  
-		stream.once('end', function() {
+	stream.once('end', function() {
         if (info.which !== 'TEXT')
-		{
-          //console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
+	{
+          	//console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
 		  result.push(prefix, inspect(Imap.parseHeader(buffer)));
 		  console.log(JSON.stringify(result));	  
-		  }
+	}
         else
-		{
-          console.log(prefix + 'Body [%s] Finished', inspect(info.which));
-		  }
+	{
+          	console.log(prefix + 'Body [%s] Finished', inspect(info.which));
+	}
 		  
-		});
-		
-		});
+	});
+	});
 		
     });
     f.once('error', function(err) {
-      console.log('Fetch error: ' + err);
+      	console.log('Fetch error: ' + err);
     });
     f.once('end', function() {
-      console.log('Done fetching all messages!');
-      imap.end();
-	  return result;
+      	console.log('Done fetching all messages!');
+      	imap.end();
+	return result;
 	  
     });
   });
